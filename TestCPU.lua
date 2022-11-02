c7977e49db83524de3f36272c2682dec4cf5c9f7
SuSuNhan <117073265+SuSuNhan@users.noreply.github.com>
2022-11-02 22:06:54 +0700

    Update TestCPU.lua

--- TestCPU.lua
+++ TestCPU.lua
@@ -353,7 +353,7 @@ do -- Default Commands
         if _PERF then return end
         
         _PERF = true
-        _TARGETFPS = 8
+        _TARGETFPS = 6
 
         if Message and tonumber(Message) then
             _TARGETFPS = tonumber(Message)
@@ -361,19 +361,19 @@ do -- Default Commands
 
         local OldLevel = settings().Rendering.QualityLevel
 
-        RunService:Set3dRenderingEnabled(false)
+        RunService:Set3dRenderingEnabled(true)
         settings().Rendering.QualityLevel = 1
 
         InputService.WindowFocused:Connect(function()
             RunService:Set3dRenderingEnabled(true)
             settings().Rendering.QualityLevel = OldLevel
-            setfpscap(60)
+            setfpscap(6)
         end)
 
         InputService.WindowFocusReleased:Connect(function()
             OldLevel = settings().Rendering.QualityLevel
 
-            RunService:Set3dRenderingEnabled(false)
+            RunService:Set3dRenderingEnabled(true)
             settings().Rendering.QualityLevel = 1
             setfpscap(_TARGETFPS)
         end)
